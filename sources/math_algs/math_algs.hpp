be6720c33293afa77b2f709f8d2a4898bd5dfcd1
Iaroslav Bird <iaroslav.pash@yandex.ru>
2025-03-15 15:12:43 +0300

    GCD algo added and pthread linking changed

--- sources/math_algs/math_algs.hpp
+++ sources/math_algs/math_algs.hpp
@@ -11,3 +11,5 @@ unsigned long long binpow(unsigned long long a, unsigned long long n);
 unsigned long long binpow(unsigned long long a, unsigned long long n, unsigned long long module);
 
 vector<unsigned long long> primes_gen(unsigned long long mini=1000, unsigned long long maxi=1000000);
+
+unsigned long long gcd(unsigned long long a, unsigned long long b);
